be522a309129f636cb14dd89ee1ba34a9825f927
zzlwte <zzlwte@gmail.com>
2024-12-24 00:01:01 +0800

    feat: crond

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,4 @@ COPY . /app
 RUN npm i
 
 EXPOSE 3000
-CMD ["npm", "run", "start:docker"]
+CMD ["npm", "run", "start:prod"]
--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   environment: process.env.NODE_ENV,
   database: {
-    dbName: 'blog',
+    dbName: process.env.MYSQL_DATABASE,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.MYSQL_ROOT_PASSWORD,
     protocol: 'http',
     logging: false,
     timezone: '+08:00'
